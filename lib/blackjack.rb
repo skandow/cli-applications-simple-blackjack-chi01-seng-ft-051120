9e3a395d782ebadea57aee39512a679729d68cd8
Steven Kandow <kando1sr@gmail.com>
2020-03-26 16:19:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -70,7 +70,7 @@ def runner
   welcome
   first_round = initial_round
   display_card_total(first_round)
-  decision = hit?(initial_round)
+  decision = hit?(first_round)
   until decision > 21 do 
     if decision == initial_round
       hit?
